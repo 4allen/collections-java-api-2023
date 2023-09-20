abb0d6218be1765c455eedcfa62662119ed23e5f
hellen atanasio <l.atanasio@hotmail.com>
2023-09-20 11:22:40 -0300

    Update list

---
+++ src/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,14 @@
+package list.OperacoesBasicas;
+
+public class Tarefa {
+    private String deescricao;
+
+    public Tarefa(String descricao){
+        this.deescricao = descricao;
+    }
+
+    public String getDeescricao() {
+        return deescricao;
+    }
+
+}
